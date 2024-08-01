b62bd00ec35b442103cb54ec9b513329ad82f692
kozzztia <kozzztia@gmail.com>
2024-08-01 10:41:27 +0300

    start

--- start.sh
+++ start.sh
@@ -4,12 +4,16 @@ then
   exit 1
 fi
 
-git init
+# git init
+# git add .
+# git commit -m "$1"
+# git branch -M main
+# git remote add origin git@github.com:kozzztia/flex-lesson.git
+# git push -u origin main
+
 git add .
 git commit -m "$1"
-git branch -M main
-git remote add origin git@github.com:kozzztia/flex-lesson.git
-git push -u origin main
+git push
 
 echo "Всё готово!"
 
